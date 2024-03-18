070542042404f2220912b0e555ebe99ca7ff364a
ashpatni20 <ash.patni20@gmail.com>
2024-03-18 23:17:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+const DadJokesContainer = document.querySelector(".DadJokesContainer");
+const jokesSection = document.querySelector(".jokesSection");
+const btn = document.querySelector(".btn");
+
+
+
+
+async function fetchJokes() {
+    try {
+        jokesSection.innerText = "Updating...";
+        btn.disabled = true;
+        btn.innerText = "Loading..";
+
+        const response = await fetch(`https://api.api-ninjas.com/v1/dadjokes?limit=1`, {
+            method: 'GET',
+            headers: {
+                'X-Api-Key': 'x4PCQJMrn/OQV1SvpqcXtA==HYvi85ubHL8MKdqf',
+                // 'Content-Type': 'application/json'
+            }
+        });
+        const result = await response.json();
+        jokesSection.innerText = result[0].joke;
+        btn.disabled = false;
+        btn.innerText = "Tell me a Joke";
+        return result;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+btn.addEventListener('click' , fetchJokes);
+
